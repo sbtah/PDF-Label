c9a0176beeb12df84964861ad767d8f293c403d5
Sbtah <79242553+sbtah@users.noreply.github.com>
2021-05-29 09:35:42 +0200

    Update main.py with file sharing functionality.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from article import Article
 from generator import Generator
+from file_share import FileSharer
+from datetime import date
+
+# Get current day.
+today = date.today()
 
 # Get input from a user. Hardcoded values just  for test.
+# Article 1.
 art_1_name = "NZ SKUWACZ D/LODU TRZON"  # input("1st article's name: ")
 art_1_ean = "5906083358784"  # input("1st article's ean code: ")
 art_1_price = "29,99"  # input("1st article's unit price: ")
 
+# Article 2.
 art_2_name = "SZCZOT DRUCIANA 3RZEDOWA"  # input("2nd article's name: ")
 art_2_ean = "5906083069307"  # input("2nd article's ean code: ")
 art_2_price = "19,99"  # input("1st article's unit price: ")
@@ -15,7 +22,12 @@ promotion_title = "WYPRZEDAŻ"  # input("Promotion: ").upper()
 # Generate Data for a file.
 new_article = Article(name = art_1_name, ean = art_1_ean, cena = art_1_price)
 new_article2 = Article(name = art_2_name, ean = art_2_ean, cena = art_2_price)
-new_file = Generator("ET1.pdf")
+
+new_file = Generator(f"Etykieta-{today}.pdf")
 new_file.generate_barcode(new_article.ean)
+
 new_file.generate_barcode(new_article2.ean)
 new_file.pdf_ety(art1=new_article, art2=new_article2, promotion=promotion_title)
+
+file_sharer = FileSharer(f"Files/Etykieta-{today}.pdf").share()
+print(file_sharer)
